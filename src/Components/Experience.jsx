acf2a1d5ea187afcd224002ff710b86a5340c85b
pani2004 <debashispani61@gmail.com>
2024-06-12 01:00:13 +0530

    Updated the tech stack

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -4,66 +4,88 @@ import tailwind from '../assets/tailwind.png'
 import javascript from '../assets/javascript.png'
 import reactImage from '../assets/reactImage.png'
 import cpp from '../assets/cpp.png'
+import express from '../assets/express.png'
+import mongo from '../assets/mongo.png'
+import node from '../assets/node.png'
 import Github from '../assets/Github.png'
+
 const Experience = () => {
     const techs = [
         {
-            id:1,
-            src:html,
-            title:'HTML',
-            style:'shadow-orange-500'
+            id: 1,
+            src: html,
+            title: 'HTML',
+            style: 'shadow-orange-500'
         },
         {
-            id:2,
-            src:tailwind,
-            title:'Tailwind',
-            style:'shadow-sky-400'
+            id: 2,
+            src: tailwind,
+            title: 'Tailwind',
+            style: 'shadow-sky-400'
         },
         {
-            id:3,
-            src:javascript,
-            title:'JavaScript',
-            style:'shadow-yellow-500'
+            id: 3,
+            src: javascript,
+            title: 'JavaScript',
+            style: 'shadow-yellow-500'
         },
         {
-            id:4,
-            src:reactImage,
-            title:'React',
-            style:'shadow-blue-600'
+            id: 4,
+            src: reactImage,
+            title: 'React',
+            style: 'shadow-blue-600'
         },
         {
-            id:5,
-            src:cpp,
-            title:'C++',
-            style:'shadow-blue-800'
+            id: 5,
+            src: express,
+            title: 'ExpressJs',
+            style: 'shadow-gray-500'
         },
         {
-            id:6,
-            src:Github,
-            title:'GitHub',
-            style:'shadow-gray-400'
+            id: 6,
+            src: mongo,
+            title: 'MongoDB',
+            style: 'shadow-green-500'
+        },
+        {
+            id: 7,
+            src: node,
+            title: 'NodeJs',
+            style: 'shadow-green-400'
+        },
+        {
+            id: 8,
+            src: cpp,
+            title: 'C++',
+            style: 'shadow-blue-800'
+        },
+        {
+            id: 9,
+            src: Github,
+            title: 'GitHub',
+            style: 'shadow-gray-400'
         }
     ]
-  return (
-    <div name = "Experience" id="Experience" className=' bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
-        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
-            <div>
-                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
-                <p className='py-6'>These are the technologies I have worked with</p>
-            </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+    return (
+        <div name="Experience" id="Experience" className='bg-gradient-to-b from-gray-800 to-black w-full'>
+            <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+                <div>
+                    <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
+                    <p className='py-6'>These are the Technologies I have worked with</p>
+                </div>
 
-                {techs.map(({id,src,title,style})=>(
-                     <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                     <img src={src} alt="" className='w-20 mx-auto'/>
-                     <p className='mt-4'>{title}</p>
-                     </div>
-                ))}
+                <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+                    {techs.map(({ id, src, title, style }) => (
+                        <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                            <img src={src} alt={title} className='w-20 mx-auto' />
+                            <p className='mt-4'>{title}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Experience
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                    I'm a ReactJS Developer
+                    I'm a Full Stack Developer
                 </h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                     I love to work on Web applications using technologies like
-                    ReactJS, Tailwind CSS, Vanilla Javascript
+                    ReactJs, Tailwind CSS, ExpressJs and MongoDB
                 </p>
                 <div>
                     <Link to="Portfolio" smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
